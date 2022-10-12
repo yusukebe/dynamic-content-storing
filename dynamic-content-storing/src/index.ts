981bc9f355e67d0c5727d14467d369f60d5e3ae8
Yusuke Wada <yusuke@kamawada.com>
2022-10-12 14:26:04 +0900

    feat: implement stale

--- dynamic-content-storing/src/index.ts
+++ dynamic-content-storing/src/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import type { Context } from 'hono'
 
 type Bindings = {
   DYNAMIC_CONTENT_STORE: KVNamespace
@@ -18,30 +20,59 @@ const headersToRecord = (headers: Headers) => {
   return kv
 }
 
+const ttl = 60
+const staleTtl = 60 * 60
+
 const app = new Hono<{ Bindings: Bindings }>()
+
+app.use('*', logger())
+
+const createCache = async (c: Context, originURL: string, staleResponse: Response) => {
+  staleResponse = staleResponse.clone()
+  let body = staleResponse.body
+  let headers = staleResponse.headers
+
+  console.log(`store stale: ${originURL}`)
+  await c.env.DYNAMIC_CONTENT_STORE.put(`stale: ${originURL}`, body, {
+    expirationTtl: staleTtl,
+    metadata: { headers: headersToRecord(headers) },
+  })
+
+  const response = await fetch(originURL)
+  body = response.body
+  headers = response.headers
+
+  console.log(`store fresh: ${originURL}`)
+  await c.env.DYNAMIC_CONTENT_STORE.put(`fresh: ${originURL}`, body, {
+    expirationTtl: ttl,
+    metadata: { headers: headersToRecord(headers) },
+  })
+}
 
 app.get('/posts/', async (c) => {
   const originURL = `https://${c.env.ORIGIN_HOST}/posts/`
 
-  let { value, metadata } = await c.env.DYNAMIC_CONTENT_STORE.getWithMetadata<Metadata>(originURL)
+  console.log(`try to get ${originURL} from kv`)
+  let { value, metadata } = await c.env.DYNAMIC_CONTENT_STORE.getWithMetadata<Metadata>(
+    `fresh: ${originURL}`
+  )
   let response: Response
 
   if (value && metadata) {
     console.log(`${originURL} already is stored`)
     response = new Response(value, { headers: metadata.headers })
   } else {
-    console.log(`fetch from ${originURL}`)
-    response = await fetch(originURL)
-    const { body, headers } = response.clone()
-    if (body) {
-      console.log(`store ${originURL}`)
-      c.executionCtx.waitUntil(
-        c.env.DYNAMIC_CONTENT_STORE.put(originURL, body, {
-          expirationTtl: 60 * 60,
-          metadata: { headers: headersToRecord(headers) },
-        })
-      )
+    let { value, metadata } = await c.env.DYNAMIC_CONTENT_STORE.getWithMetadata<Metadata>(
+      `stale: ${originURL}`
+    )
+    if (value && metadata) {
+      console.log(`${originURL} is expired but the stale is found`)
+      response = new Response(value, { headers: metadata.headers })
+    } else {
+      console.log(`fetch from ${originURL}`)
+      response = await fetch(originURL)
     }
+    c.executionCtx.waitUntil(createCache(c, originURL, response))
   }
 
   return response
@@ -49,7 +80,8 @@ app.get('/posts/', async (c) => {
 
 app.delete('/posts/', async (c) => {
   const originURL = `https://${c.env.ORIGIN_HOST}/posts/`
-  c.executionCtx.waitUntil(c.env.DYNAMIC_CONTENT_STORE.delete(originURL))
+  await c.env.DYNAMIC_CONTENT_STORE.delete(`fresh: ${originURL}`)
+  await c.env.DYNAMIC_CONTENT_STORE.delete(`stale: ${originURL}`)
   return c.redirect('/posts/')
 })
 
